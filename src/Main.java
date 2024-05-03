91649d134536e875a1707017c2946c1a67304942
Pavel <pavelpulle1@gmail.co>
2024-05-03 07:58:36 +0300

    Домашние задание исправил

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class Main {
 
     //Задача №1
-    private static void checkYearIsLeapAndPrint(int leapYear) {
+    private static void checkYearIsLeap(int leapYear) {
         boolean leapYearConditions = (leapYear % 400 == 0) || (leapYear % 4 == 0 && leapYear % 100 != 0);
         boolean validYear = leapYear >= 1584;
         if (validYear && leapYearConditions) {
@@ -14,50 +14,63 @@ public class Main {
     }
 
     // Задача №2
-    private static String validateDeviceAndYear(int deviceType, int year) {
+    private static void validateDeviceAndYear(int deviceType, int year) {
+        int ios =0;
+        int android=1;
         int currentYear = LocalDate.now().getYear();
-        if (deviceType == 0 && year < currentYear) {
-            return ("«Установите облегченную версию приложения для iOS по ссылке».");
-        } else if (deviceType == 0 && year >= currentYear) {
-            return ("«Установите версию приложения для iOS по ссылке».");
-        } else if (deviceType == 1 && year < currentYear) {
-            return ("«Установите облегченную версию приложения для Android по ссылке».");
-        } else if (deviceType == 1 && year >= currentYear) {
-            return ("«Установите версию приложения для Android по ссылке».");
+        if (deviceType == ios && year < currentYear) {
+            System.out.println ("«Установите облегченную версию приложения для iOS по ссылке».");
+        } else if (deviceType == ios && year >= currentYear) {
+            System.out.println ("«Установите версию приложения для iOS по ссылке».");
+        } else if (deviceType == android && year < currentYear) {
+            System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
+        } else if (deviceType == android && year >= currentYear) {
+            System.out.println("«Установите версию приложения для Android по ссылке».");
         } else {
-            return ("Такая система не поддерживается");
+            System.out.println ("Такая система не поддерживается");
         }
     }
 
 
     //  Задача №3
-    private static String calculateDeliveryDistance(int distance) {
-        if (distance > 100) {
-            return "Wrong distance";
+    private static void deliveryDistance(int deliveryDistance) {
+        int days = 1;
+
+        if (deliveryDistance <= 20) {
+            System.out.printf("Пoтpeбуется дней:" + days);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            days = days + 1;
+            System.out.println("Пoтpeбуется дней: " + days);
+
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            days = days +=2;
+            System.out.println("Пoтpe6yeтcя дней: " + days);
+
         } else {
-            int i = (int) Math.ceil((double) (distance - 20) / 40) + 1;
-            return i + "";
+            System.out.println("Доставки нет");
         }
     }
+
 
 
     public static void main(String[] args) {
 
         System.out.println("Задача №1_____________________________________________________________________");
         int year = 2021;
-        checkYearIsLeapAndPrint(year);
+        checkYearIsLeap(year);
 
 
         System.out.println("\nЗадача №2_____________________________________________________________________");
         int clientOS = 1;
         int clentDeviceYear = 2024;
-        String operatingSystem = validateDeviceAndYear(clientOS, clentDeviceYear);
-        System.out.println(operatingSystem );
+        validateDeviceAndYear(clientOS, clentDeviceYear);
+
 
 
         System.out.println("\nЗадание №3_____________________________________________________________________");
-        String string = calculateDeliveryDistance(100);
-        System.out.printf("«Потребуется дней: %s » ",string);
+        deliveryDistance(95);
+
+
 
 
 
