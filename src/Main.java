7354344c6744a207d15fd8eaed810bc50782946d
Pavel <pavelpulle1@gmail.co>
2024-05-05 22:23:16 +0300

    Задача 2. Домашка готова.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class Main {
 
     //Задача №1
-    private static void  checkYearIsLeapAndPrint(int year) {
+    private static void checkYearIsLeapAndPrint(int year) {
         boolean leapYearConditions = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
         boolean validYear = year >= 1584;
         if (validYear && leapYearConditions) {
@@ -14,20 +14,20 @@ public class Main {
     }
 
     // Задача №2
-    private static void validateDeviceAndYear(int clentDeviceYear, int year) {
-        int ios =0;
-        int android=1;
+    private static void validateDeviceAndYear(int clientDeviceYear, int year) {
+        int ios = 0;
+        int android = 1;
         int currentYear = LocalDate.now().getYear();
-        if (clentDeviceYear == ios && year < currentYear) {
-            System.out.println ("«Установите облегченную версию приложения для iOS по ссылке».");
-        } else if (clentDeviceYear == ios && year >= currentYear) {
-            System.out.println ("«Установите версию приложения для iOS по ссылке».");
-        } else if (clentDeviceYear == android && year < currentYear) {
+        if (clientDeviceYear == ios && year < currentYear) {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
+        } else if (clientDeviceYear == ios && year >= currentYear) {
+            System.out.println("«Установите версию приложения для iOS по ссылке».");
+        } else if (clientDeviceYear == android && year < currentYear) {
             System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
-        } else if (clentDeviceYear == android && year >= currentYear) {
+        } else if (clientDeviceYear == android && year >= currentYear) {
             System.out.println("«Установите версию приложения для Android по ссылке».");
         } else {
-            System.out.println ("Такая система не поддерживается");
+            System.out.println("Такая система не поддерживается");
         }
     }
 
@@ -38,21 +38,20 @@ public class Main {
         String deliveryTime = " + срок доставки.";
 
         if (deliveryDistance <= 20) {
-            System.out.printf("Пoтpeбуется дней:" + days+deliveryTime);
+            System.out.printf("Пoтpeбyeтcя дней:" + days + deliveryTime);
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
             days = days + 1;
-            System.out.println("Пoтpeбуется дней: " + days+deliveryTime);
+            System.out.println("Пoтpeбyeтcя дней: " + days + deliveryTime);
 
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            days+=2;
-            System.out.println("Пoтpe6yeтcя дней: " + days+deliveryTime);
+            days += 2;
+            System.out.println("Пoтpeбyeтcя дней: " + days + deliveryTime);
 
         } else {
             System.out.println("Доставки нет");
         }
 
     }
-
 
 
     public static void main(String[] args) {
@@ -64,16 +63,12 @@ public class Main {
 
         System.out.println("\nЗадача №2_____________________________________________________________________");
         int clientOS = 1;
-        int clentDeviceYear = 2024;
-        validateDeviceAndYear(clientOS, clentDeviceYear);
-
+        int clientDeviceYear = 2015;
+        validateDeviceAndYear(clientOS, clientDeviceYear);
 
 
         System.out.println("\nЗадание №3_____________________________________________________________________");
         deliveryDistance(95);
-
-
-
 
 
     }
